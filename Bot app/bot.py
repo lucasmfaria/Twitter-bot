36b09cea2ca0ec459d757716a85fca255794e8bc
Lucas Faria <lucas.meneghesso.faria@gmail.com>
2019-07-09 18:06:50 -0300

    Update bot.py
    Close open DB connections and new data types for the DB columns

--- Bot app/bot.py
+++ Bot app/bot.py
@@ -26,7 +26,6 @@ def unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=response_message)
 
 def hashtag_search(bot, update):
-    #hashtag = '#machinelearning'
     hashtag = update.message.text
     twitter = twitter_auth()
     tweets = getTweets(twitter, hashtag, n_tweets)
@@ -35,16 +34,16 @@ def hashtag_search(bot, update):
     session = cluster.connect()
     session.execute("CREATE KEYSPACE IF NOT EXISTS botdb WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }")
     session.set_keyspace('botdb')
-    #session.execute("CREATE TABLE IF NOT EXISTS tweet (id int primary key, created_at timestamp, txt text, user_name text, user_followers_count int, lang text)")
-    
-    #Todos campos com texto----alterar
-    session.execute("CREATE TABLE IF NOT EXISTS tweet (id text primary key, created_at text, txt text, user_name text, user_followers_count text, lang text)")
+    session.execute("CREATE TABLE IF NOT EXISTS tweet (id bigint primary key, created_at text, txt text, user_name text, user_followers_count int, lang text, hashtag_ text)")
     
     for tweet in tweets:
         response_message = response_message + "Usuário: " + tweet._json['user']['name'] + "\nTweet: " + tweet._json['text'] + "\n\n"
-        session.execute("""INSERT INTO tweet (id, created_at, txt, user_name, user_followers_count, lang) values (%s, %s, %s, %s, %s, %s)""", (tweet._json['id_str'], str(tweet._json['created_at']), tweet._json['text'], tweet._json['user']['name'], str(tweet._json['user']['followers_count']), tweet._json['lang']))
+        session.execute("""INSERT INTO tweet (id, created_at, txt, user_name, user_followers_count, lang, hashtag_) values (%s, %s, %s, %s, %s, %s, %s)""", (tweet._json['id'], str(tweet._json['created_at']), tweet._json['text'], tweet._json['user']['name'], tweet._json['user']['followers_count'], tweet._json['lang'], hashtag))
     
+    #Returns the messages for the bot. Just for debugging for now.
     bot.send_message(chat_id=update.message.chat_id, text=response_message)
+    session.shutdown()
+    cluster.shutdown()
 
 def main():
     updater = Updater(token=bot_token)
